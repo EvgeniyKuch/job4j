0ade0a53a16fba8f084a1f662571bcf6c8a55518
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-05-10 20:46:38 +0500

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+ *Класс Calculate предназанчен для вывода строки
+ *"Hello, world!" в консоль.
+ *@author Kuchumov
+ *@since 09.09.2018
+ */
+
 public class Calculate{
+	
+	/**
+	 *Метод main предназанчен для вывода строки
+     *"Hello, world!" в консоль.
+	 *@param args - args
+	 */
 	
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
