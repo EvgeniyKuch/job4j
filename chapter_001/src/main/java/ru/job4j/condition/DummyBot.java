b0532410a53805346bc9fb2d4519a8fa11ee773c
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-05-16 19:01:57 +0500

    3.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * Глупый бот.
+ *@author Kuchumov (geka.1985@mail.ru)
+ *@version $Id$
+ *@since 16.05.2018
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
