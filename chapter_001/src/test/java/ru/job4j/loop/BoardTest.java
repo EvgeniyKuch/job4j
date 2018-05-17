57bb7825c84e5158c67d03fbeac4a34b6a79882a
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-05-17 19:01:27 +0500

    4.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест шахматной доски.
+ */
+public class BoardTest {
+
+    /**
+     * Шахматная доска 3х3.
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.lineSeparator();
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Шахматная доска 5х4.
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        //напишите здесь тест, проверяющий формирование доски 5 на 4.
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.lineSeparator();
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
+        assertThat(result, is(expected));
+    }
+}
