a6ea1bf138e9a374b53dfd7ec6cc02fdeb7cf7bd
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-05-18 18:13:03 +0500

    4.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,97 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * PaintTest
+ *@author Kuchumov
+ *@version $Id$
+ *@since 17.05.2018
+ */
+public class PaintTest {
+
+    /**
+     * Правый треугольник.
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Левый треугольник.
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Пирамида высотой 4.
+     */
+    @Test
+    public void whenPyramid4Height() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Пирамида высотой 6.
+     */
+    @Test
+    public void whenPyramid6Height() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(6);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("     ^     ")
+                                .add("    ^^^    ")
+                                .add("   ^^^^^   ")
+                                .add("  ^^^^^^^  ")
+                                .add(" ^^^^^^^^^ ")
+                                .add("^^^^^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
