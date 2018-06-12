130b8852331c72cfe4161ca28618492d204af855
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-06-12 22:08:24 +0500

    4.6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,11 +1,46 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.StringJoiner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    private String menu() {
+        return new StringJoiner(System.lineSeparator())
+                .add("")
+                .add("Меню.")
+                .add("0. Добавить новую заявку")
+                .add("1. Показать все заявки")
+                .add("2. Редактировать заявку")
+                .add("3. Удалить заявку")
+                .add("4. Поиск заявки по id")
+                .add("5. Поиск заявок по имени")
+                .add("6. Выйти из программы")
+                .toString();
+    }
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     /**
      * Тест добавления новой заявки.
@@ -42,5 +77,94 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         Item expect = null;
         assertThat(tracker.findById(itemOne.getId()), is(expect));
+    }
+
+    /**
+     * Тест для вывода списка всех заявок.
+     */
+    @Test
+    public void whenShowAllItems() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "test desc"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add(this.menu())
+                                .add("")
+                                .add("------------ Список заявок начало --------------")
+                                .add("")
+                                .add("Имя: test name")
+                                .add("Описание: test desc")
+                                .add("Создана: " + new Date(item.getCreated()))
+                                .add("ID: " + item.getId())
+                                .add("------------ Список заявок конец --------------")
+                                .add(this.menu())
+                                .add(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Тест для поиска заявки по ID.
+     */
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "test desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add(this.menu())
+                                .add("")
+                                .add("------------ Поиск заявки по id --------------")
+                                .add("------------- Найденная заявка ---------------")
+                                .add("")
+                                .add("Имя: test name")
+                                .add("Описание: test desc")
+                                .add("Создана: " + new Date(item.getCreated()))
+                                .add("ID: " + item.getId())
+                                .add("------------- Найденная заявка ---------------")
+                                .add(this.menu())
+                                .add(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Тест для поиска заявок по имени.
+     */
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "test desc"));
+        Input input = new StubInput(new String[]{"5", "test name", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add(this.menu())
+                                .add("")
+                                .add("------------ Поиск заявки по имени --------------")
+                                .add("---------- Найденные заявки начало --------------")
+                                .add("")
+                                .add("Имя: test name")
+                                .add("Описание: test desc")
+                                .add("Создана: " + new Date(item.getCreated()))
+                                .add("ID: " + item.getId())
+                                .add("---------- Найденные заявки конец --------------")
+                                .add(this.menu())
+                                .add(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 }
