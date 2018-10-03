2401b39e44f1bd993f3fbd79431798f2f0e3375a
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-10-03 14:42:52 +0500

    1. Пин-понг. [#1016]

---
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,36 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        double x = this.rect.getX();
+        double y = this.rect.getY();
+        double deltaX = Math.random() * 20 - 10;
+        double deltaY = Math.random() * 20 - 10;
+        while (true) {
+            x += deltaX;
+            y += deltaY;
+            if (x > 290.0 || x < 0.0) {
+                deltaX = -deltaX;
+            }
+            if (y > 290.0 || y < 0.0) {
+                deltaY = -deltaY;
+            }
+            this.rect.setX(x);
+            this.rect.setY(y);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
