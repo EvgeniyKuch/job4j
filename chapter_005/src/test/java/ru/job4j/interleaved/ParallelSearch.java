28e894e924e6705f01186a1284a8f09c87cd124f
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2018-11-09 21:26:43 +0500

    2. Обеспечить остановку потребителя. [#66825]

--- chapter_005/src/test/java/ru/job4j/interleaved/ParallelSearch.java
+++ chapter_005/src/test/java/ru/job4j/interleaved/ParallelSearch.java
@@ -2,29 +2,34 @@ package ru.job4j.interleaved;
 
 public class ParallelSearch {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
-        final Thread consumer = new Thread(
+        final Thread consumer = new Thread (
                 () -> {
                     while (!Thread.currentThread().isInterrupted()) {
+                        try {
                             System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
         );
         consumer.start();
-        new Thread(
+        final Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
-                        queue.offer(index);
                         try {
+                            queue.offer(index);
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
-                    consumer.interrupt();
                 }
-
-        ).start();
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
     }
 }
