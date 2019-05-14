59f9e0afa8fcfcd6f8b69ebfa5fd9226a530b3a8
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2019-05-14 14:09:34 +0500

    Удалить дубликаты через SQL [#106936]

---
+++ chapter_006/src/main/java/ru/job4j/duplicates/duplicates_cities.sql
@@ -0,0 +1,11 @@
+﻿-- Удалить дубликаты через SQL
+--create table cities (
+--	id serial primary key,
+--	name varchar(255)
+--);
+
+delete from cities 
+	where id not in (
+		select min(id) from cities
+		group by name
+);
