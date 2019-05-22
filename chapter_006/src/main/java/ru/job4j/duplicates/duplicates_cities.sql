cb15689cb6f3fd4a4da5e1a001037a1fc0e0811b
EvgeniyKuch <evgeniy.kuchumov@gmail.com>
2019-05-22 11:52:08 +0500

    Удалить дубликаты через SQL [#106936]

--- chapter_006/src/main/java/ru/job4j/duplicates/duplicates_cities.sql
+++ chapter_006/src/main/java/ru/job4j/duplicates/duplicates_cities.sql
@@ -1,11 +1,54 @@
 ﻿-- Удалить дубликаты через SQL
---create table cities (
---	id serial primary key,
---	name varchar(255)
---);
 
+-- Создаём таблицу
+create table if not exists cities (
+	id serial primary key,
+	name varchar(255)
+);
+delete from cities;
+
+-- Вносим исходные данные
+insert into cities(name) values('Москва');
+insert into cities(name) values('СПб');
+insert into cities(name) values('Казань');
+insert into cities(name) values('Москва');
+insert into cities(name) values('СПб');
+insert into cities(name) values('Казань');
+insert into cities(name) values('Москва');
+insert into cities(name) values('СПб');
+insert into cities(name) values('Казань');
+insert into cities(name) values('Москва');
+insert into cities(name) values('СПб');
+insert into cities(name) values('Казань');
+
+-- Исходное состояние таблицы
+select * from cities;
+-- id |  name
+------+-------
+--  1 | Москва
+--  2 | СПб
+--  3 | Казань
+--  4 | Москва
+--  5 | СПб
+--  6 | Казань
+--  7 | Москва
+--  8 | СПб
+--  9 | Казань
+-- 10 | Москва
+-- 11 | СПб
+-- 12 | Казань
+
+-- Удаляем дубликаты
 delete from cities 
 	where id not in (
 		select min(id) from cities
 		group by name
 );
+
+-- Состояние таблицы после удаления дубликатов
+select * from cities;
+-- id |  name
+------+--------
+--  1 | Москва
+--  2 | СПб
+--  3 | Казань
